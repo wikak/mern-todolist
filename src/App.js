f06b2b387190fe6918a12f5f01ba63ca3228bb35
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-11 22:19:03 +0000

    init router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateTodo from './components/create-todos.component'
+import TodosList from './components/todos-list.component'
+import EditTodo from './components/edit-todos.component'
+import 'bulma/css/bulma.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <section class="hero is-warning">
+          <div class="hero-body">
+            <div class="container">
+              <div class="columns">
+                <div class="column">
+                  <h1 class="title">
+                    MEARN - STACK
+                    </h1>
+                </div>
+                <div class="column">
+                  <Route path="/" exact component={TodosList} />
+                  <Route path="/edit/:id" exact component={EditTodo} />
+                  <Route path="/create" exact component={CreateTodo} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Router>
+    )
+  }
+
 }
 
 export default App;
