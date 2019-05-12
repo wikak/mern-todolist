c6477dc27aad16feb42384ed2bd66adeb51d506c
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-12 00:32:24 +0000

    form ok

--- src/App.js
+++ src/App.js
@@ -3,31 +3,59 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CreateTodo from './components/create-todos.component'
 import TodosList from './components/todos-list.component'
 import EditTodo from './components/edit-todos.component'
+import logo from './logo.svg'
 import 'bulma/css/bulma.css'
-
+import './App.css'
 class App extends Component {
   render() {
     return (
       <Router>
-        <section class="hero is-warning">
-          <div class="hero-body">
-            <div class="container">
-              <div class="columns">
-                <div class="column">
-                  <h1 class="title">
-                    MEARN - STACK
-                    </h1>
-                </div>
-                <div class="column">
-                  <Route path="/" exact component={TodosList} />
-                  <Route path="/edit/:id" exact component={EditTodo} />
-                  <Route path="/create" exact component={CreateTodo} />
-                </div>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+              <img src={logo} width="54px" height="54px" alt="mern" />
+            </a>
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <Link to="/" className="navbar-item">Todos</Link>
+              <Link to="/create" className="navbar-item">Create Todos</Link>
+            </div>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">
+                  Log in
+                        </a>
               </div>
             </div>
           </div>
+        </nav>
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-three-quarters">
+                <Route path="/" exact component={TodosList} />
+                <Route path="/edit/:id" exact component={EditTodo} />
+                <Route path="/create" exact component={CreateTodo} />
+              </div>
+              <div className="column"></div>
+              <div className="column"></div>
+            </div>
+
+          </div>
         </section>
-      </Router>
+      </Router >
     )
   }
 
--- src/components/create-todos.component.js
+++ src/components/create-todos.component.js
@@ -1,11 +1,97 @@
 import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todo_description: "",
+            todo_responsible: "",
+            todo_priority: "",
+            todo_complete: false,
+        }
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this)
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this)
+        this.onChangepriority = this.onChangepriority.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onChangeTodoDescription(e) {
+        this.setState({ todo_description: e.target.value })
+    }
+    onChangeTodoResponsible(e) {
+        this.setState({ todo_responsible: e.target.value })
+    }
+    onChangepriority(e) {
+        this.setState({ todo_priority: e.target.value })
+    }
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log("Form Submission");
+        console.log(`todo Description: ${this.state.todo_description}`);
+        console.log(`todo Responsible: ${this.state.todo_responsible}`);
+        console.log(`todo Priority: ${this.state.todo_priority}`);
+        console.log(`todo Complete: ${this.state.todo_complete}`);
+
+        this.setState({
+            todo_description: "",
+            todo_responsible: "",
+            todo_priority: "",
+            todo_complete: false,
+        })
+    }
     render() {
         return (
-            <div>
-                <p>Welcome to Create Todo Component! </p>
-            </div>
+            <form onSubmit={this.onSubmit}>
+                <div className="field">
+                    <div className="control has-icons-left">
+                        <label className="label">Description</label>
+                        <div className="control">
+                            <input
+                                className="input"
+                                type="text"
+                                placeholder="todo Description"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Responsible</label>
+                    <div className="control has-icons-left has-icons-right">
+                        <input
+                            className="input"
+                            placeholder="todo Responsible"
+                            value={this.state.todo_responsible}
+                            onChange={this.onChangeTodoResponsible} />
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Priority</label>
+                    <div className="control">
+                        <div className="select">
+                            <select
+                                value={this.state.todo_priority}
+                                onChange={this.onChangepriority}
+                            >
+                                <option value="">Choose</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-success">Submit</button>
+                    </div>
+                    <div className="control">
+                        <button className="button is-text">Cancel</button>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
